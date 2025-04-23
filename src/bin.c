dd3e9915ea915b4e5ab124018945ded1d1d31d48
Michael du Plessis <mdp0101@gmail.com>
2025-04-23 11:34:33 +0200

    added function to get the size of a bin

--- src/bin.c
+++ src/bin.c
@@ -3,6 +3,12 @@
 #include <stddef.h>
 #include <stdio.h>
 
+// gets the amount of memory the bin takes up
+// including the meta data from the bin size
+size_t size_of_bin(size_t bin_object_size) {
+  return sizeof(Bin) + sizeof(BITMASK) * 8 * bin_object_size;
+}
+
 size_t find_first_free_bit(BITMASK bitmask) {
   // loop through each bit of the bitmask
   for (size_t i = 0; i < sizeof(bitmask) * 8; i++) {
@@ -16,18 +22,20 @@ size_t find_first_free_bit(BITMASK bitmask) {
 }
 
 // creates a bin of a specified size
+// and sets all the default parameters
 Bin *create_bin(size_t size) {
   size_t bin_size = sizeof(Bin);
 
   // allocating memory to fit bins size
-  // since the total amount of memory needed is the size of the bitmask + the
+  // since the total amount of memory needed is the size of the bin + the
   // amount of objects which is the number of bits in the bitmask further
   // multiplied by the size of the objects that the bin must hold
   // TODO: Look into padding this for alignment
-  Bin *bin = (Bin *)request_block(bin_size + sizeof(BITMASK) * 8 * size);
+  Bin *bin = (Bin *)request_block(size_of_bin(size));
   *bin = (Bin) {
    .mask = 0,
    .next = NULL,
+   // moving pointer to just after the metadata
    .ptr = (void *)(bin + 1), 
   };
 
@@ -56,7 +64,10 @@ void *bin_alloc(BinManager *bin_manager, size_t bin_size) {
   // marking the spot as taken
   current->mask |= 1 << spot;
   // returning the memory
-  // this is done by moving from the pointer to the memory the index of where a free spot is
+  // this is done by moving from the ptr to the memory the index of where a free spot is
   // multiplied by how much space a spot takes up
+  //
+  // In better words its from where the region of memory starts + the index/spot where the
+  // memory is being allocated multiplied by the size each object takes
   return (void *)((char *)current->ptr + spot * bin_size);
 }
