dee0eb3d64816c52a0724e8eb7ec908e10cfaccf
Michael du Plessis <mdp0101@gmail.com>
2025-04-23 09:29:52 +0200

    Checking that the correct memory address is assigned to variables

--- main.c
+++ main.c
@@ -1,13 +1,17 @@
 #include "src/allocator.h"
 #include <stdio.h>
 
-int main() {
-  int *x = malloc(sizeof(int));
-  int *y = malloc(sizeof(int));
+#define TESTING_TYPE int
 
-  printf("Size of int is %lu.\n", sizeof(int));
+int main() {
+  TESTING_TYPE *x = malloc(sizeof(TESTING_TYPE));
+  TESTING_TYPE *y = malloc(sizeof(TESTING_TYPE));
+
+  printf("Size of type is %lu.\n", sizeof(TESTING_TYPE));
   printf("Memory address of x: %p.\n", x);
   printf("Memory address of y: %p.\n", y);
+  printf("Memory address of x: %lu.\n", (uintptr_t)x);
+  printf("Memory address of y: %lu.\n", (uintptr_t)y);
 
   *x = 42;
   *y = 7;
