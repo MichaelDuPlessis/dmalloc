7fa17ef2bd49fc94a85fe5eab28bf16e8c651ac5
Michael du Plessis <mdp0101@gmail.com>
2025-04-28 15:59:51 +0200

    started reimplementing bins

--- src/bin.c
+++ src/bin.c
@@ -1,77 +1,53 @@
 #include "bin.h"
-#include "distributer.h"
-#include <stddef.h>
-#include <stdio.h>
+#include "bitset.h"
+#include "mmap_allocator.h"
 
-// gets the amount of memory the bin takes up
-// including the meta data from the bin size
-size_t size_of_bin(size_t bin_object_size) {
-  return sizeof(Bin) + sizeof(BITMASK) * 8 * bin_object_size;
+// Calculates the number of blocks that can fit in some amount of memory
+size_t calculate_num_blocks(size_t block_size, size_t total_memory) {
+  return total_memory / block_size;
 }
 
-size_t find_first_free_bit(BITMASK bitmask) {
-  // loop through each bit of the bitmask
-  for (size_t i = 0; i < sizeof(bitmask) * 8; i++) {
-    // check if the bit is free (0)
-    if ((bitmask & (1 << i)) == 0) {
-      return i; // return the index of the first free bit
-    }
+// Calculates the number of bits needed for the bitset
+size_t calculate_bitset_size(size_t block_size) {
+  // the amount of memory availble
+  // since I don't want to include th bitsets values
+  size_t total_memory_available = PAGE_SIZE - (sizeof(Bin) - sizeof(BitSet));
+
+  size_t total_blocks =
+      calculate_num_blocks(block_size, total_memory_available);
+  // size of bitset required
+  size_t bitset_size = size_of_bitset(total_blocks);
+
+  // after accounting for the size of the bitset figure out how many blocks
+  // can now be allocated
+  size_t new_total_blocks =
+      calculate_num_blocks(block_size, total_memory_available - bitset_size);
+
+  // if the new total number of blocks is the same as the old we have converged
+  while (new_total_blocks != total_blocks) {
+    new_total_blocks =
+        calculate_num_blocks(block_size, total_memory_available - bitset_size);
+
+    // getting the new
+    bitset_size = size_of_bitset(new_total_blocks);
+    total_blocks = new_total_blocks;
   }
 
-  return -1; // return -1 if no free bits are found
+  return total_blocks;
 }
 
-// creates a bin of a specified size
-// and sets all the default parameters
-Bin *create_bin(size_t size) {
-  size_t bin_size = sizeof(Bin);
+void init_bin(Bin *bin, size_t bin_size) {
+  // setting the size of the bin
+  bin->bin_size = bin_size;
+  bin->next = NULL;
 
-  // allocating memory to fit bins size
-  // since the total amount of memory needed is the size of the bin + the
-  // amount of objects which is the number of bits in the bitmask further
-  // multiplied by the size of the objects that the bin must hold
-  // TODO: Look into padding this for alignment
-  Bin *bin = (Bin *)request_block(size_of_bin(size));
-  *bin = (Bin) {
-   .mask = 0,
-   .next = NULL,
-   // moving pointer to just after the metadata
-   .ptr = (void *)(bin + 1), 
-  };
+  // calculating the size of the bitset
+  size_t num_bits = calculate_bitset_size(bin_size);
 
-  return bin;
+  // initializing bitset
+  // since the bitset has to at the end this is a clever way to get its start
+  init_bitset(((BitSet *)(bin + 1)) - sizeof(BitSet), num_bits);
+
+  // Setting where the initial memory used for allocation begins
+  bin->ptr = (void *)(sizeof(Bin) + size_of_bitset(num_bits) - sizeof(BitSet));
 }
-
-void *bin_alloc(BinManager *bin_manager, size_t bin_size) {
-  // finding the current bin with free memory
-  Bin *current = bin_manager->head;
-  // if the current pointer is null it means we have reached the end of the list and therefore
-  // must allocate more memory
-  while (current && current->mask == BITMASK_MAX) {
-    current = current->next;
-  }
-
-  // checking if current has memory associated with it
-  if (!current) {
-    Bin *bin = create_bin(bin_size);
-    bin->next = bin_manager->head;
-    bin_manager->head = bin;
-    current = bin;
-  }
-
-  // finding the spot where to allocate the memory
-  size_t spot = find_first_free_bit(current->mask);
-  // marking the spot as taken
-  current->mask |= 1 << spot;
-  // returning the memory
-  // this is done by moving from the ptr to the memory the index of where a free spot is
-  // multiplied by how much space a spot takes up
-  //
-  // In better words its from where the region of memory starts + the index/spot where the
-  // memory is being allocated multiplied by the size each object takes
-  return (void *)((char *)current->ptr + spot * bin_size);
-}
-
-// void *bin_free(BinManager *bin_manager, size_t bin_size) {
-  // finding bin that stored the memory
-// }
--- src/bin.h
+++ src/bin.h
@@ -2,20 +2,18 @@
 #define BIN_H
 
 #include <stddef.h>
-
-// this type is used to represent the free spots in the region of memory
-#define BITMASK unsigned long
-// this is the max size of the type used to reprsent the bitmask
-#define BITMASK_MAX (BITMASK)0 - 1
+#include "bitset.h"
 
 // A bin and all its metadata
 typedef struct Bin {
-  // the free spots in the bin
-  BITMASK mask;
   // the memory where items are allocated
   void *ptr;
   // the next bin
   struct Bin *next;
+  // the size of the objects allocated in the bin
+  size_t bin_size;
+  // the free spots in the bin
+  BitSet bitset;
 } Bin;
 
 // this struct is responsible for managing a specifically sized bin
@@ -24,7 +22,7 @@ typedef struct {
   Bin *head;
 } BinManager;
 
-// allocates memory from a bin
-void *bin_alloc(BinManager *bin_manager, size_t bin_size);
+// initializes the bin
+void init_bin(Bin *bin, size_t bin_size);
 
 #endif
