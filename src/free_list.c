f84aa35681f6ec34025b7c73da0ca85d3194b659
Michael du Plessis <mdp0101@gmail.com>
2025-05-13 18:10:25 +0200

    started implementing free list alloc

--- src/free_list.c
+++ src/free_list.c
@@ -3,17 +3,18 @@
 #include "mmap_allocator.h"
 #include <stddef.h>
 
-// A header for a block of memory
+// A header for a block of allocated memory
 typedef struct {
   // The amount of memory allocated for a block
-  // not including the header
+  // including the header
   size_t size;
-} BlockHeader;
+} AllocHeader;
 
 // A block of memory
 typedef struct Block {
-  // Metadata about the allocation
-  BlockHeader header;
+  // The amount of memory available that can be allocated
+  // this includes the memory that the block occupies
+  size_t size;
   // Where the next free block is located
   struct Block *next;
 } Block;
@@ -28,8 +29,57 @@ typedef struct Chunk {
   struct Chunk* next;
   // Where the first free block of memory is
   Block* block_head;
-  // The amount of memory the chunk has left after the header
-  size_t size;
-  // The amount of memory that is free
-  size_t free_bytes;
 } Chunk;
+
+// The head of the chunks
+static Chunk *chunk_head = NULL;
+
+// initializes a block
+static inline void init_block(Block *block, size_t size, Block *next) {
+  block->size = size;
+  block->next = next;
+}
+
+static inline void init_alloc_header(AllocHeader *header, size_t size) {
+  header->size = size;
+}
+
+void *free_list_alloc(size_t size) {
+  Chunk *current_chunk = chunk_head;
+
+  // Iterate over chunks
+  while (current_chunk) {
+    // if there is enough space left in the chunk
+    Block *current_block = current_chunk->block_head;
+
+    // iterating over blocks
+    while (current_block) {
+      // if there is enough space in the node allocate memory 
+      if (current_block->size >= size) {
+        // since there is enough memory split block and allocate memory
+        AllocHeader *header = (AllocHeader *)current_block;
+        void *ptr = (void *)(header + 1); // moving to just after the BlockHeader
+
+        // the amount of memory remaining
+        size_t remaining = current_block->size - (sizeof(AllocHeader) + size);
+
+        // if there is enough space create a new block
+        // TODO: If the amount of memory left is less than or equal to
+        // the amount of memory the bin allocator uses this can be skipped
+        if (remaining > sizeof(Block)) {
+          // since there is space allocate memory
+          Block *new_block = (Block *)((char *)header + sizeof(AllocHeader) + size);
+          init_block(new_block, remaining, current_block->next);
+
+          // this is for later when the header's size is assigned
+          remaining = 0;
+        }
+
+        // setting headers parameters
+        init_alloc_header(header, sizeof(AllocHeader) + size + remaining);
+
+        return ptr;
+      }
+    }
+  }
+}
--- src/free_list.h
+++ src/free_list.h
@@ -3,7 +3,8 @@
 
 #include <stddef.h>
 
-// Allocates memory to the free list
+// Allocates memory to the free list.
+// It uses a first fit algorithm
 void *free_list_alloc(size_t size);
 
 // Deallocates memory from the free list
